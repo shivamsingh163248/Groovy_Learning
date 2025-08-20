d8c1e5d34b51c1e3fdaf2f546c6728b09150f529
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-08-20 21:48:24 +0530

    Adding the Detatype file

---
+++ src/com/test/demo/datatype.groovy
@@ -0,0 +1,45 @@
+package com.test.demo
+
+import java.awt.geom.Arc2D.Float
+
+// try directly without using the class and main function 
+short s = 100 ; 
+println s   ; 
+println Short.MIN_VALUE  ; 
+println Short.MAX_VALUE ; 
+
+print("============")
+
+
+long l = 100000 ; 
+println l ; 
+println Long.MIN_VALUE
+println Long.MAX_VALUE
+
+
+
+// check and learning for the integer 
+int i = 10 ; 
+println i  ; 
+println Integer.MIN_VALUE ; 
+println Integer.MAX_VALUE ; 
+
+
+// now check for the float 
+float f = 10.5F
+println f ; 
+// also print the minimum  values and the maximum values 
+
+
+// check for the char 
+char c  ='A'
+print(c) ; 
+
+// boolean values have the only true and false values 
+boolean a = true ; 
+boolean b = false ; 
+
+// only boolean values store the true and false values 
+println a ; 
+println b ; 
+
