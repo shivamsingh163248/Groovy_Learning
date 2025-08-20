1ac528bf21152241654f6b4a48f6d36c66941056
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-08-20 20:49:57 +0530

    update

--- src/com/test/demo/Varaibles.groovy
+++ src/com/test/demo/Varaibles.groovy
@@ -4,30 +4,70 @@ class Varaibles {
 
 	static void main(args) {
 		// TODO Auto-generated method stub
-		def Name = "AMD" ; 
-		String company = "amd" ; 
-	  
-		// try for the print using the variable and concatenation  
-		println("the company name is ${Name}") ; 
-		println("SAP LAB "+company)
+//		def Name = "AMD" ; 
+//		String company = "amd" ; 
+//	  
+//		// try for the print using the variable and concatenation  
+//		println("the company name is ${Name}") ; 
+//		println("SAP LAB "+company)
+//		
+//		// it is still working file not a actually problems 
+//		// try for the int variable 
+//		int Number = 78 ; 
+//		print(Number)
+//        // and check for the printing 
+//		
+//		
+//		// now learning the Assignment 
+//		def(int A,int B, int C) = [40,50]
+//	    // check for the printing 
+//		println(A) ; 
+//		print(B) ; 
+//		print(C) ; 
+//		
+//		// CREATING and Assign Variable 
+//		def(String Company_name , int Company_number , int values ) = [45,78,65]
+//		// check for the print all the variables 
+//		println(Company_name) ; 
+//		println(Company_number)
+//		println(values)
+//		
+//		
+//		// again try for one less variable 
+//		def(String nam , int num , int sh) = [6,7]
+//        // try the print the variable 
+//		print(nam) ; 
+//		println(num) ; 
+//		println(sh) ; 		
 		
-		// it is still working file not a actually problems 
-		// try for the int variable 
-		int Number = 78 ; 
-		print(Number)
-        // and check for the printing 
-		
-		
-		// now learning the Assignment 
-		def(A,B,C) = [40,50]
-	    // check for the printing 
-		println(A) ; 
-		print(B) ; 
-		print(C) ; 
-		
-		// CREATING and Assign Variable 
-		def(String Company_name , int Company_number , values ) = [45,78,65]
-		// check for the print all the variables 
+		// Basic variable assignment
+		def name = "AMD"
+		String company = "amd"
+
+		// Printing variables
+		println("The company name is ${name}")
+		println("SAP LAB " + company)
+
+		// Integer variable
+		int number = 78
+		println(number)
+
+		// Correct destructuring assignment
+		def (a, b) = [40, 50]
+		println(a)
+		println(b)
+
+		// If you want 3 vars, give 3 values
+		def (x, y, z) = [45, 78, 65]
+		println(x)
+		println(y)
+		println(z)
+
+		// If fewer values than vars → missing ones become null
+		def (p, q, r) = [6, 7]
+		println(p)   // 6
+		println(q)   // 7
+		println(r)   // null
 		
 	}
 }
