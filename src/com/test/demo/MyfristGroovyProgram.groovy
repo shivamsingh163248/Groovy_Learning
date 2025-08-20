11560e55e4564655feacde14199fac8afce91cae
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-08-20 20:13:58 +0530

    Learning the groovy Language

--- src/com/test/demo/MyfristGroovyProgram.groovy
+++ src/com/test/demo/MyfristGroovyProgram.groovy
@@ -6,7 +6,10 @@ class MyfristGroovyProgram {
 	static void main(args) {
 		
 		
-		println("this is the my frist program ")
+		println("this is the my frist program ") ; 
+		println "this is my  second line of print " ; 
+		println('this is the my frist program ') ; 
 		
 	}
 }
+ 
---
+++ src/com/test/demo/Varaibles.groovy
@@ -0,0 +1,33 @@
+package com.test.demo
+
+class Varaibles {
+
+	static void main(args) {
+		// TODO Auto-generated method stub
+		def Name = "AMD" ; 
+		String company = "amd" ; 
+	  
+		// try for the print using the variable and concatenation  
+		println("the company name is ${Name}") ; 
+		println("SAP LAB "+company)
+		
+		// it is still working file not a actually problems 
+		// try for the int variable 
+		int Number = 78 ; 
+		print(Number)
+        // and check for the printing 
+		
+		
+		// now learning the Assignment 
+		def(A,B,C) = [40,50]
+	    // check for the printing 
+		println(A) ; 
+		print(B) ; 
+		print(C) ; 
+		
+		// CREATING and Assign Variable 
+		def(String Company_name , int Company_number , values ) = [45,78,65]
+		// check for the print all the variables 
+		
+	}
+}
