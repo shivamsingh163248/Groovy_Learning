7573e319a61699f62210e4601e8b5411da03fa5f
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-08-21 14:24:14 +0530

    learning the string in the groovy for the ci and cd automation

---
+++ src/com/test/demo/String.groovy
@@ -0,0 +1,22 @@
+package com.test.demo
+
+import com.test.demo.String
+
+class String {
+
+	static void main(args) {
+		// TODO Auto-generated method stub
+		
+		// double "" string and single quests string 
+		// triple single quoted quoted quoted string 
+		// triple double  quoted 
+		def name = "shivam" ; 
+		println name ; 
+		println "my name is "+"shivam" ; 
+		println "my second name is ".concat("shivam") ; 
+		println "my name is $name" ; 
+		println "my name is ${name}"  ; 
+		
+		
+	}
+}
